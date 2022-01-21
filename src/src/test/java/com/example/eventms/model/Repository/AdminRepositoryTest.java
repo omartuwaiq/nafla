812cd128c8eeacd23cacc8591ecdad3e1187b626
omartuwaiq <91540868+omartuwaiq@users.noreply.github.com>
2022-01-22 00:55:21 +0300

    Delete src directory

--- src/src/test/java/com/example/eventms/model/Repository/AdminRepositoryTest.java
+++
@@ -1,6 +0,0 @@
-package com.example.eventms.model.Repository;
-
-class AdminRepositoryTest {
-}
-
-//Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
